0a4f5c254a5eded1a7eae524fe8910f015461fd7
tamimkmz <140689794+tamimkmz@users.noreply.github.com>
2023-10-17 23:12:46 -0700

    Update UniStrCollection.java

--- src/HW25and26/UniStrCollection.java
+++ src/HW25and26/UniStrCollection.java
@@ -4,15 +4,25 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class UniStrCollection {
+   public class UniStrCollection {
     public static void main(String[] args) {
-        Set<String>uniqueString=new LinkedHashSet<>();
-        uniqueString.add("Hello");
-        uniqueString.add("Java");
-        uniqueString.add("Progaramming");
-        uniqueString.add("is fun");
-        uniqueString.add("Java");
+        Set<String> uniqueStrings = new LinkedHashSet<>();
+        uniqueStrings.add("Hello");
+        uniqueStrings.add("Java");
+        uniqueStrings.add("Programming"); // Fixed spelling mistake
+        uniqueStrings.add("is fun");
+        uniqueStrings.add("Java");
+
         String concatenatedString = concatenateStrings(uniqueStrings);
         System.out.println(concatenatedString);
     }
 
+    public static String concatenateStrings(Set<String> strings) {
+        StringBuilder sb = new StringBuilder();
+        for (String s : strings) {
+            sb.append(s);
+            sb.append(" ");
+        }
+        return sb.toString();
+    }
 }
